4fecaaddcd70158452330c1974e048caa047974d
Hassaan Mohamed <160543413+thehoho@users.noreply.github.com>
2024-07-23 22:38:28 +0300

    Add files via upload

---
+++ 12r.js
@@ -0,0 +1,166 @@
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+updateScoreElement();
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    document.querySelector(".js-auto-play-button").innerHTML = "Stop playing";
+    isAutoPlaying = true;
+  } else {
+    document.querySelector(".js-auto-play-button").innerHTML = "Auto play";
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+}
+
+document.querySelector(".js-rock-button").addEventListener("click", () => {
+  playGame("rock");
+});
+
+document.querySelector(".js-paper-button").addEventListener("click", () => {
+  playGame("paper");
+});
+
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "q") {
+    playGame("rock");
+  } else if (event.key === "w") {
+    playGame("paper");
+  } else if (event.key === "e") {
+    playGame("scissors");
+  } else if (event.key === "a") {
+    autoPlay();
+  } else if (event.key === "Backspace" || "Esc") {
+    showResetConfirmation();
+  }
+});
+
+document.querySelector(".js-scissors-button").addEventListener("click", () => {
+  playGame("scissors");
+});
+
+document.querySelector(".js-auto-play-button").addEventListener("click", () => {
+  autoPlay();
+});
+
+document.querySelector(".js-rest-button").addEventListener("click", () => {
+  showResetConfirmation();
+});
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  let result = "";
+
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You lose.";
+    } else if (computerMove === "paper") {
+      result = "You win.";
+    } else if (computerMove === "scissors") {
+      result = "Tie.";
+    }
+  }
+
+  if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You win.";
+    } else if (computerMove === "paper") {
+      result = "Tie.";
+    } else if (computerMove === "scissors") {
+      result = "You lose.";
+    }
+  }
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie.";
+    } else if (computerMove === "paper") {
+      result = "You lose.";
+    } else if (computerMove === "scissors") {
+      result = "You win.";
+    }
+  }
+
+  if (result === "You win.") {
+    score.wins += 1;
+  } else if (result === "You lose.") {
+    score.losses += 1;
+  } else if (result === "Tie.") {
+    score.ties += 1;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+
+  updateScoreElement();
+
+  document.querySelector(".js-result").innerHTML = result;
+  document.querySelector(
+    ".js-moves"
+  ).innerHTML = `      You <img src="img/${playerMove}-emoji.png" class="move-icon" />
+<img src="img/${computerMove}-emoji.png" class="move-icon" /> Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = "";
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
+  }
+  return computerMove;
+}
+function showResetConfirmation() {
+  document.querySelector(".js-reset-confirmation").innerHTML = `
+      Are you sure you want to reset the score?
+      <button class="js-reset-confirm-yes reset-confirm-button">
+        Yes
+      </button>
+      <button class="js-reset-confirm-no reset-confirm-button">
+        No
+      </button>
+    `;
+
+  document
+    .querySelector(".js-reset-confirm-yes")
+    .addEventListener("click", () => {
+      resetScore();
+      hideResetConfirmation();
+    });
+
+  document
+    .querySelector(".js-reset-confirm-no")
+    .addEventListener("click", () => {
+      hideResetConfirmation();
+    });
+}
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem("score");
+  updateScoreElement();
+}
+
+function hideResetConfirmation() {
+  document.querySelector(".js-reset-confirmation").innerHTML = "";
+}
